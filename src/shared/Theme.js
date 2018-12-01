acfdc575ac913277ab10f967bd33ef23cc1ba55d
vitormalencar <vitor.maia.alencar@gmail.com>
2018-12-01 23:44:54 +0100

    fix typo

--- src/shared/Theme.js
+++ src/shared/Theme.js
@@ -3,7 +3,7 @@ const theme = {
   secondary: '#9F6EFF',
   primaryDark: '#0042cb',
   primaryLight: '#969bff',
-  secondarydDark: '#6b40cb',
+  secondaryDark: '#6b40cb',
   secondaryLight: '#d49eff'
 }
 
