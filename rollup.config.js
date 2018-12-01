1367947e20e90fb7e320489c933cf52ade9ed961
vitormalencar <vitor.maia.alencar@gmail.com>
2018-12-02 00:17:06 +0100

    updating rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,24 @@
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
 import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
 import url from 'rollup-plugin-url'
+import { terser } from 'rollup-plugin-terser'
 
 import pkg from './package.json'
 
+const globals = { 'styled-components': 'styled' }
+
+const output = [
+  { file: pkg.main, format: 'cjs', sourcemap: true },
+  { file: pkg.module, format: 'es', sourcemap: true }
+].map(r => ({ ...r, globals }))
+
 export default {
+  external: ['styled-components'],
   input: 'src/index.js',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      sourcemap: true
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true
-    }
-  ],
+  output,
   plugins: [
     external(),
     postcss({
@@ -32,6 +30,6 @@ export default {
       plugins: ['external-helpers']
     }),
     resolve(),
-    commonjs()
+    commonjs(),
   ]
 }
