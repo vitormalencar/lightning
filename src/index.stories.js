c5222e81e07a7d49111f2d443412860d5c6751e6
vitormalencar <vitor.maia.alencar@gmail.com>
2018-12-01 23:35:52 +0100

    little cleaning

--- src/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import ExampleComponent from './index';
-
-let stories = storiesOf('ExampleComponent', module)
-
-stories.add('Default', () => <ExampleComponent text='From Storybook'/>)
