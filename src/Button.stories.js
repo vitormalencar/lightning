8a1364b9be9291044d3cead814c19b42c520aebb
vitormalencar <vitor.maia.alencar@gmail.com>
2018-12-02 23:06:01 +0100

    removing  button  marging

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,40 +1,62 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { ThemeProvider } from "styled-components";
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { ThemeProvider } from 'styled-components'
+import styled from 'styled-components'
+import { ButtonLight, Button, ButtonBlock, ButtonBlockLight } from './Button'
+import theme from './shared/Theme'
 
-import { ButtonLight, Button, ButtonBlock, ButtonBlockLight } from "./Button";
-import theme from "./shared/Theme";
+let stories = storiesOf('Buttons', module)
 
-let stories = storiesOf("Buttons", module);
+const variants = Object.keys(theme)
 
-const variants = Object.keys(theme);
+const Spacer = styled.div`
+  margin: 1rem;
+`
 
-stories.add("Button", () => {
-  return <div>{variants.map(i => <Button theme={i}>Button {i}</Button>)}</div>;
-});
-
-stories.add("Button light", () => {
-  return (
-    <div>
-      {variants.map(i => <ButtonLight theme={i}>Button {i}</ButtonLight>)}
-    </div>
-  );
-});
-
-stories.add("Button block", () => {
-  return (
-    <div>
-      {variants.map(i => <ButtonBlock theme={i}>Button {i}</ButtonBlock>)}
-    </div>
-  );
-});
-
-stories.add("Button block light", () => {
+stories.add('Button', () => {
   return (
     <div>
       {variants.map(i => (
-        <ButtonBlockLight theme={i}>Button {i}</ButtonBlockLight>
+        <Spacer>
+          <Button theme={i}>Button {i}</Button>
+        </Spacer>
       ))}
     </div>
-  );
-});
+  )
+})
+
+stories.add('Button light', () => {
+  return (
+    <div>
+      {variants.map(i => (
+        <Spacer>
+          <ButtonLight theme={i}>Button {i}</ButtonLight>
+        </Spacer>
+      ))}
+    </div>
+  )
+})
+
+stories.add('Button block', () => {
+  return (
+    <div>
+      {variants.map(i => (
+        <Spacer>
+          <ButtonBlock theme={i}>Button {i}</ButtonBlock>
+        </Spacer>
+      ))}
+    </div>
+  )
+})
+
+stories.add('Button block light', () => {
+  return (
+    <div>
+      {variants.map(i => (
+        <Spacer>
+          <ButtonBlockLight theme={i}>Button {i}</ButtonBlockLight>
+        </Spacer>
+      ))}
+    </div>
+  )
+})
