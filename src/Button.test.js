c6c5187e3c2409498dc49c8b31756d71bdfffc6a
vitormalencar <vitor.maia.alencar@gmail.com>
2018-12-02 16:57:18 +0100

    adding test to button component

---
+++ src/Button.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import renderer from "react-test-renderer";
+import "jest-styled-components";
+
+import { Button, DarkenMixing } from "./Button";
+import theme from "./shared/Theme";
+
+test("Renders with the default primary color", () => {
+  const tree = renderer.create(<Button />).toJSON();
+  expect(tree).toMatchSnapshot();
+  expect(tree).toHaveStyleRule("background-color", theme.primary);
+});
+
+test("changes the css props base on the theme ", () => {
+  const tree = renderer.create(<Button theme={"secondary"} />).toJSON();
+  expect(tree).toMatchSnapshot();
+  expect(tree).toHaveStyleRule("background-color", theme.secondary);
+  expect(tree).toHaveStyleRule("border-color", theme.secondary);
+});
+
+test("changes the color on hover", () => {
+  const tree = renderer.create(<Button />).toJSON();
+  const darken = DarkenMixing("primary");
+  expect(darken).toBe("#001bf4");
+  expect(tree).toHaveStyleRule("border-color", "#001bf4", {
+    modifier: ":hover"
+  });
+  expect(tree).toHaveStyleRule("background-color", "#001bf4", {
+    modifier: ":hover"
+  });
+});
