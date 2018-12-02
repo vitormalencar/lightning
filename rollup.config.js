e33506542d4c61fac6b70bc5fa38b42935301599
vitormalencar <vitor.maia.alencar@gmail.com>
2018-12-02 13:51:17 +0100

    adding svg support

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,23 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
-import external from 'rollup-plugin-peer-deps-external'
-import postcss from 'rollup-plugin-postcss'
-import url from 'rollup-plugin-url'
-import { terser } from 'rollup-plugin-terser'
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+import resolve from "rollup-plugin-node-resolve";
+import external from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import url from "rollup-plugin-url";
+import svgr from "@svgr/rollup";
 
-import pkg from './package.json'
+import pkg from "./package.json";
 
-const globals = { 'styled-components': 'styled' }
+const globals = { "styled-components": "styled" };
 
 const output = [
-  { file: pkg.main, format: 'cjs', sourcemap: true },
-  { file: pkg.module, format: 'es', sourcemap: true }
-].map(r => ({ ...r, globals }))
+  { file: pkg.main, format: "cjs", sourcemap: true },
+  { file: pkg.module, format: "es", sourcemap: true }
+].map(r => ({ ...r, globals }));
 
 export default {
-  external: ['styled-components'],
-  input: 'src/index.js',
+  external: ["styled-components"],
+  input: "src/index.js",
   output,
   plugins: [
     external(),
@@ -25,11 +25,12 @@ export default {
       modules: true
     }),
     url(),
+    svgr(),
     babel({
-      exclude: 'node_modules/**',
-      plugins: ['external-helpers']
+      exclude: "node_modules/**",
+      plugins: ["external-helpers"]
     }),
     resolve(),
-    commonjs(),
+    commonjs()
   ]
-}
+};
